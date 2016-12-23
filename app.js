36dbef4f9eacf15416646d90984b08d381139b8e
Shawn-Chen <twschen@tw.ibm.com>
2016-12-24 07:34:48 +0800

    all

--- app.js
+++ app.js
@@ -13,7 +13,33 @@ var Cloudant   = require('cloudant');
 var username = process.env.cloudant_username || "ae237f43-4430-42cc-8ed1-32fc5be0a3d4-bluemix";
 var password = process.env.cloudant_password || "cbb00dcad3028eb85084f61ef44d750dab44e5bf4625222a6d48d8ae64dc6724";
 var cloudant = Cloudant({account:username, password:password});
-var matchDB0 = cloudant.db.use('matchDB0');
+var matchDBmale0 = cloudant.db.use('matchdbmale0');
+var matchDBfemale0 = cloudant.db.use('matchdbfemale0');
+
+// My data
+var matchDATAmale0 = {
+    "teams" : [
+      ["Randy", null],
+      ["Kelvin", null], 
+      ["啊 吉", null],
+      ["SKC", null], 
+      ["Henry", null], 
+      ["Shawn", null],
+      ["啊德", "Kevin"],
+      ["Figo", null]
+    ],
+    "results" : [
+	    [
+	      [[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null],[null, null]],
+	      [[null, null],[null, null],[null, null],[null, null]],
+	      [[null, null],[null, null]],
+	      [[null, null]]
+	    ],
+	    [
+	      []
+	    ]
+    ]
+}
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -34,11 +60,27 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   console.log("server starting on " + appEnv.url);
 });
 
-app.post('/match0', function(req, res, next) {
+app.post('/bracketmale0', function(req, res, next) {
+  matchDATAmale0 = req.body.bracket;
+  hbDB.insert({data: matchDATAmale0, timestamp: new Date().getTime()}, function(err, body){
+    if(!err){
+      console.log(body);
+    }
+  });
+});
+
+app.post('/bracketfemale0', function(req, res, next) {
   hbDB.insert({data: myHeartbeat, timestamp: new Date().getTime()}, function(err, body){
     if(!err){
       console.log(body);
     }
   });
-  res.send("heartbeat OK");
+});
+
+app.get('getmale0', function(req, res, next) {
+	res.send(matchDATAmale0);
+});
+
+app.get('getfemale0', function(req, res, next) {
+	res.send(matchDATAmale0);
 });
