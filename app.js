afce799201ded3f0d36e48c3d8e0eca594d23d68
Shawn-Chen <twschen@tw.ibm.com>
2016-12-24 11:59:30 +0800

    all

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var username = process.env.cloudant_username || "ae237f43-4430-42cc-8ed1-32fc5be
 var password = process.env.cloudant_password || "cbb00dcad3028eb85084f61ef44d750dab44e5bf4625222a6d48d8ae64dc6724";
 var cloudant = Cloudant({account:username, password:password});
 var matchDBmale0 = cloudant.db.use('matchdbmale0');
+var matchDBchamp0 = cloudant.db.use('matchdbchamp0');
 var matchDBfemale0 = cloudant.db.use('matchdbfemale0');
 
 // My data
@@ -36,11 +37,74 @@ var matchDATAmale0 = {
 	      [[null, null]]
 	    ],
 	    [
-	      []
+	      [[null, null],[null, null],[null, null],[null, null]],
+	      [[null, null],[null, null],[null, null],[null, null]],
+	      [[null, null],[null, null]],
+	      [[null, null],[null, null]],
+	      [[null, null]]
 	    ]
     ]
-}
-
+};
+var matchDATAchamp0 = {
+    "teams" : [
+      ["", ""],
+      ["", ""]
+    ],
+    "results" : [
+	    [
+	      [[null, null],[null, null]],
+	      [[null, null]]
+	    ]
+    ]
+};
+var matchDATAfemale0 = {
+  "teams": [
+    {
+      "id": 1,
+      "name": "小美"
+    },
+    {
+      "id": 2,
+      "name": "家瑜"
+    },
+    {
+      "id": 3,
+      "name": "大貓"
+    }
+  ],
+  "matches": [
+    {
+      "a": {
+        "team": 0,
+        "score": null
+      },
+      "b": {
+        "team": 1,
+        "score": null
+      }
+    },
+    {
+      "a": {
+        "team": 0,
+        "score": null
+      },
+      "b": {
+        "team": 2,
+        "score": null
+      }
+    },
+    {
+      "a": {
+        "team": 1,
+        "score": null
+      },
+      "b": {
+        "team": 2,
+        "score": null
+      }
+    }
+  ]
+};
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
@@ -48,6 +112,10 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+var bodyParser = require('body-parser');
+// Configure Express
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
@@ -60,27 +128,54 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   console.log("server starting on " + appEnv.url);
 });
 
+// for testing(refresh)
+matchDBmale0.insert({data: matchDATAmale0, timestamp: new Date().getTime()}, function(err, body){
+    if(!err){
+      //console.log(body);
+    }
+});
+matchDBmale0.insert({data: matchDATAchamp0, timestamp: new Date().getTime()}, function(err, body){
+    if(!err){
+      //console.log(body);
+    }
+});
+
 app.post('/bracketmale0', function(req, res, next) {
-  matchDATAmale0 = req.body.bracket;
-  hbDB.insert({data: matchDATAmale0, timestamp: new Date().getTime()}, function(err, body){
-    if(!err){
-      console.log(body);
-    }
-  });
+	matchDATAmale0 = req.body.bracket;
+	matchDBmale0.insert({data: matchDATAmale0, timestamp: new Date().getTime()}, function(err, body){
+	    if(!err){
+	      //console.log(body);
+	    }
+	});
+	res.send("OK");
 });
-
+app.post('/bracketchamp0', function(req, res, next) {
+	matchDATAchamp0 = req.body.bracket;
+	matchDBchamp0.insert({data: matchDATAchamp0, timestamp: new Date().getTime()}, function(err, body){
+	    if(!err){
+	      //console.log(body);
+	    }
+	});
+	res.send("OK");
+});
 app.post('/bracketfemale0', function(req, res, next) {
-  hbDB.insert({data: myHeartbeat, timestamp: new Date().getTime()}, function(err, body){
+  matchDBfemale0.insert({data: myHeartbeat, timestamp: new Date().getTime()}, function(err, body){
     if(!err){
-      console.log(body);
+      //console.log(body);
     }
   });
+  res.send("OK");
 });
 
-app.get('getmale0', function(req, res, next) {
+app.get('/getmale0', function(req, res, next) {
+	//console.log(matchDATAmale0);
 	res.send(matchDATAmale0);
 });
 
-app.get('getfemale0', function(req, res, next) {
-	res.send(matchDATAmale0);
+app.get('/getchamp0', function(req, res, next) {
+	res.send(matchDATAchamp0);
+});
+
+app.get('/getfemale0', function(req, res, next) {
+	res.send(matchDATAfemale0);
 });
